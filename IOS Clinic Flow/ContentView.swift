f824ad3a9b6bbadefcd618b9cf7e128b64bc6f43
hasrax <hasragunawardena093@gmail.com>
2026-02-26 11:11:57 +0530

    Initial Commit

---
+++ IOS Clinic Flow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS Clinic Flow
+//
+//  Created by COBSCCOMP24.2P-023 on 2026-02-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS Clinic Flow/IOS_Clinic_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Clinic_FlowApp.swift
+//  IOS Clinic Flow
+//
+//  Created by COBSCCOMP24.2P-023 on 2026-02-26.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Clinic_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
